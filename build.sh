d4bc772b8ca706b64c7d5169401f68db25cdbfb0
Dmitry Akatov <dmitry.akatov@protonmail.com>
2026-05-11 18:03:41 +0200

    xmobar fix

--- build.sh
+++ build.sh
@@ -11,4 +11,8 @@ if [ -f "$theme" ]; then
         -e "s/fgColor = \"#[0-9A-Fa-f]*\"/fgColor = \"$FG\"/" \
         -e "s/borderColor = \"#[0-9A-Fa-f]*\"/borderColor = \"$BORDER\"/" \
         "$dir/xmobarrc"
+    # Replace hardcoded widget colors in built-in plugins (Cpu, Memory)
+    [ -n "$GOOD" ] && sed -i "s/\"#B6E63E\"/\"$GOOD\"/g; s/\"#27AE60\"/\"$GOOD\"/g" "$dir/xmobarrc"
+    [ -n "$WARN" ] && sed -i "s/\"#FFCC00\"/\"$WARN\"/g; s/\"#E67E22\"/\"$WARN\"/g" "$dir/xmobarrc"
+    [ -n "$ERR"  ] && sed -i "s/\"#E74C3C\"/\"$ERR\"/g" "$dir/xmobarrc"
 fi
